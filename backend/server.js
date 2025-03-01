6369ee09c046a113b25bc5d53507ead1ee4c4ddf
Tushar Gera <tushargera003@gmail.com>
2025-03-01 17:43:59 +0530

    vercel.json file updated

--- backend/server.js
+++ backend/server.js
@@ -33,3 +33,6 @@ app.use("/api", contactRoutes);
 app.use("/api/auth", authRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.get("/", (req, res) => {
+    res.send("Backend is working!");
+  });
