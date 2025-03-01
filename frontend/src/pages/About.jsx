9993fb1d0c2d695305f79b06815109924edc7320
Tushar Gera <tushargera003@gmail.com>
2025-03-01 18:59:31 +0530

    final commit frontend vercel.json file added FINALLY

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -15,7 +15,7 @@ const teamMembers = [
       "https://th.bing.com/th/id/OIP.GqGVPkLpUlSo5SmeDogUdwHaHa?w=191&h=191&c=7&r=0&o=5&dpr=1.3&pid=1.7",
   },
   {
-    name: "ASHUTOSH",
+    name: "TUSHAR GERA",
     role: "PROJECT MANAGER",
     image:
       "https://th.bing.com/th/id/OIP.GqGVPkLpUlSo5SmeDogUdwHaHa?w=191&h=191&c=7&r=0&o=5&dpr=1.3&pid=1.7",
