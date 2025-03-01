db13638265889f6c7c9b72a4a4f6cb78e0f326be
Tushar Gera <tushargera003@gmail.com>
2025-03-01 22:27:00 +0530

    try previous version 14

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",  // 👈 Fix asset loading issue
+  base: "",  // 👈 Fix asset loading issue
   build: {
     outDir: "dist",
   },
