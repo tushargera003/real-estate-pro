f5be9c48f4d6d9b78c8a5919daa0dc7477972ef5
Tushar Gera <tushargera003@gmail.com>
2025-03-01 18:28:35 +0530

    vercel.json file updated refresh issue fixed again

--- backend/server.js
+++ backend/server.js
@@ -34,5 +34,5 @@ app.use("/api/auth", authRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 app.get("/", (req, res) => {
-    res.send("Backend is working!");
+    res.send("Backend is working properly!");
   });
